43f2cb467dce1917a328928b5d8ffa1f3e6953b5
chocalata <cata627@gmail.com>
2025-03-15 10:50:37 +0100

    linter configured on 04 project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,71 +1,71 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState } from 'react'
 
 const FollowMouse = () => {
-	const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(false)
 
-	const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
-	useEffect(() => {
-		console.log("effect", { enabled });
-		if (!enabled) return;
+  useEffect(() => {
+    console.log('effect', { enabled })
+    if (!enabled) return
 
-		const handleMove = (event) => {
-			const { clientX, clientY } = event;
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
 
-			setPosition({ x: clientX, y: clientY });
-		};
+      setPosition({ x: clientX, y: clientY })
+    }
 
-		window.addEventListener("mousemove", handleMove);
+    window.addEventListener('mousemove', handleMove)
 
-		// cleanup:
-		// --> cuando el componente se desmonta
-		// --> cuando cambian las dependencias antes de ejecutar el efecto
-		return () => {
-			// cleanup method
-			console.log("cleanup mousemove");
-			window.removeEventListener("mousemove", handleMove);
-		};
-	}, [enabled]);
+    // cleanup:
+    // --> cuando el componente se desmonta
+    // --> cuando cambian las dependencias antes de ejecutar el efecto
+    return () => {
+      // cleanup method
+      console.log('cleanup mousemove')
+      window.removeEventListener('mousemove', handleMove)
+    }
+  }, [enabled])
 
-	useEffect(() => {
-		document.body.classList.toggle("no-cursor", enabled);
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
 
-		return () => {
-			console.log("cleanup no-cursor");
-			document.body.classList.remove("no-cursor");
-		};
-	}, [enabled]);
+    return () => {
+      console.log('cleanup no-cursor')
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
 
-	return (
-		<>
-			<div
-				style={{
-					position: "absolute",
-					backgroundColor: "rgba(0, 0, 0, 0.5)",
-					border: "1px solid #fff",
-					borderRadius: "50%",
-					opacity: 0.8,
-					pointerEvents: "none",
-					left: -25,
-					top: -25,
-					width: 50,
-					height: 50,
-					transform: `translate(${position.x}px, ${position.y}px)`,
-				}}
-			/>
-			<button onClick={() => setEnabled(!enabled)}>
-				{enabled ? "Desactivar" : "Activar"} Seguir puntero
-			</button>
-		</>
-	);
-};
-
-function App() {
-	return (
-		<main>
-			<FollowMouse />
-		</main>
-	);
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
+      </button>
+    </>
+  )
 }
 
-export default App;
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
---
+++ projects/04-react-prueba-tecnica-vanilla/src/App.jsx
@@ -0,0 +1,5 @@
+export function App() {
+  //const [fact, setFact] = useState('lorem ipsum...')
+
+  return <h1></h1>
+}
